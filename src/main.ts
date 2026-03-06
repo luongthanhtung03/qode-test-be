ee5bb0f0152d52e6419ef7a42e951ec1136d7346
luongthanhtung03 <tungdfg124@gmail.com>
2026-03-06 12:18:09 +0700

    fix: cors error again

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: process.env.CORS_ORIGIN,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
   });
   app.useGlobalPipes(
